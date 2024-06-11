d3a294a47d23208cbc2b8cebce98a656b4c191b6
Nishant Hegde <55502735+NIshant-Hegde@users.noreply.github.com>
2024-06-11 13:24:06 +0530

    Time: 635 ms (14.67%), Space: 16.6 MB (41.65%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -4,13 +4,8 @@ class Solution:
             return 1
         else:
             l = [i for i in range(1, n + 1)]
-            print(l)
 
             for i in range(1, len(l)):
-                print("x - ", l[i])
-                print("Left sum - ", sum(l[0:(i+1)]))
-                print("Right Sum - ", sum(l[i:(len(l) + 1)]))
-                print('----------')
                 if sum(l[0:(i+1)]) == sum(l[i:(len(l) + 1)]):
                     return i+1
 
