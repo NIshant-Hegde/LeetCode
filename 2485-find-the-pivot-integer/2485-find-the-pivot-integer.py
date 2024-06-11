a0376d8525b70d9602e49f6e6b94343a8cdb9c88
Nishant Hegde <55502735+NIshant-Hegde@users.noreply.github.com>
2024-06-11 18:36:29 +0530

    Time: 45 ms (60.76%), Space: 16.5 MB (41.65%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,5 +1,6 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
+        '''Brute-Force: Comparing an element's left sum and right sum
         if n == 1:
             return 1
         else:
@@ -8,5 +9,26 @@ class Solution:
             for i in range(1, len(l)):
                 if sum(l[0:(i+1)]) == sum(l[i:(len(l) + 1)]):
                     return i+1
+
+            return -1
+        '''
+        
+        #Two pointer approach
+        lp, rp = 1, n
+        lsum, rsum = 1, n
+        
+        if n == 1:
+            return 1
+        else:
+            while lp < rp:
+                if lsum < rsum:
+                    lp += 1
+                    lsum += lp
+                else:
+                    rp -= 1
+                    rsum += rp
+
+                if (lsum == rsum) and (lp == rp):
+                    return lp
 
             return -1
