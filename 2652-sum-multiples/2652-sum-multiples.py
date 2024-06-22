20347b0509252bb6d54af36d93cc2bf57ecf5b6a
Nishant Hegde <55502735+NIshant-Hegde@users.noreply.github.com>
2024-06-22 11:10:54 +0530

    Time: 67 ms (35.21%), Space: 16.4 MB (87.52%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,18 +1,12 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        hmap = [0] * 10000
-        i = 1
+        l = []
+        i = 0
         
         while i <= n:
             if ((i%3)==0) or ((i%5)==0) or ((i%7)==0):
-                print(i)
-                hmap[i] = 1
+                l.append(i)
 
             i+=1
-        
-        out = 0
-        for i, ele in enumerate(hmap):
-            if ele == 1:
-                out += i
                 
-        return out
+        return sum(l)
